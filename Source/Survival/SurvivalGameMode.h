fc6bf9e6c58a6c1c836d43e976f82edb427888c9
Jannik Szwaczka <blodjer@live.de>
2016-03-02 16:24:06 +0100

    Add blank project

---
+++ Source/Survival/SurvivalGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SurvivalGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVAL_API ASurvivalGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
