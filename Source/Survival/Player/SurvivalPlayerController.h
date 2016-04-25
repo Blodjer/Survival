9770bb6dbc31debe61647206b1031aeaec84fc3d
Jannik Szwaczka <blodjer@live.de>
2016-04-25 18:40:58 +0200

    Add player controller class

---
+++ Source/Survival/Player/SurvivalPlayerController.h
@@ -0,0 +1,16 @@
+// Copyright 2016 GAMES ACADEMY GmbH
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "SurvivalPlayerController.generated.h"
+
+UCLASS()
+class SURVIVAL_API ASurvivalPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ASurvivalPlayerController();
+
+};
