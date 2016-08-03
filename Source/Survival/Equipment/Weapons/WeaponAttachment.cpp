bbe09437725a474c63ed053700a328a8003f14e7
Jannik Szwaczka <blodjer@live.de>
2016-08-03 21:14:47 +0200

    Fixed scar ironsight collision

--- Source/Survival/Equipment/Weapons/WeaponAttachment.cpp
+++ Source/Survival/Equipment/Weapons/WeaponAttachment.cpp
@@ -8,10 +8,14 @@ AWeaponAttachment::AWeaponAttachment()
 	Mesh1P = CreateDefaultSubobject<UStaticMeshComponent>("Mesh1P");
 	Mesh1P->SetOnlyOwnerSee(true);
 	Mesh1P->SetCastShadow(false);
+	Mesh1P->SetCollisionProfileName("NoCollision");
+	Mesh1P->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	RootComponent = Mesh1P;
 
 	Mesh3P = CreateDefaultSubobject<UStaticMeshComponent>("Mesh3P");
 	Mesh3P->SetOwnerNoSee(true);
+	Mesh3P->SetCollisionProfileName("NoCollision");
+	Mesh3P->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	Mesh3P->SetupAttachment(Mesh1P);
 
 	PrimaryActorTick.bCanEverTick = false;
